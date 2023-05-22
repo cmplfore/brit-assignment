72050ae1f5105e2458ef84b44838f459daa234be
lewisforeman <lewis.foreman@heycar.co.uk>
2023-05-22 23:50:49 +0100

    Adding scenario 2 test

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -20,3 +20,16 @@ def test_home_page_search(page: Page) -> None:
         'Interim results for the six months ended 30 June 2022',
         'Gavin Wilkinson',
         'John King'])
+
+
+def test_home_page_contact_us_navigation(page: Page) -> None:
+    page.goto('https://www.britinsurance.com', wait_until='networkidle')
+    expect(page).to_have_title('Brit Insurance')
+    page.get_by_role("link", name="Allow selection").click()
+    page.locator('xpath=//*[contains(@class, "component--header__burger")]').click()
+    page.get_by_role("link", name="contact").click()
+    expect(page).to_have_title('Contact')
+
+    formatted_address = page.locator('xpath=//*[@id="bermudaoffice"]//address').inner_text().strip().replace('\n',' ')
+    assert 'Ground Floor, Chesney House The Waterfront, 96 Pitts Bay Road, Pembroke, Hamilton HM 08, Bermuda' \
+           in formatted_address
